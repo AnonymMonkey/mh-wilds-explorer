d888fdf1b2b41a36bf038e4580734ad6f6d66d8d
Andino Eichberger <93142482+AnonymMonkey@users.noreply.github.com>
2026-02-10 15:30:02 +0100

    feat: add more icons and add dialog feature for monster list

--- src/app/features/monster-list/monster-list.ts
+++ src/app/features/monster-list/monster-list.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, viewChild, ElementRef } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 import { MonsterService } from '../../core/services/monster';
+import { Monster } from '../../core/models/monster.model';
 
 @Component({
   selector: 'app-monster-list',
@@ -14,4 +16,35 @@ export class MonsterList {
   monsterResource = rxResource({
     stream: () => this.monsterService.getAll(),
   });
+
+  handleImageError(event: Event): void {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = 'assets/MHWilds-Database-main/monster/assets/icons/Unknown_Icon.png';
+  }
+
+  getMonsterImagePath(name: string): string {
+    const formattedName = name
+      .replace(/ /g, '_')
+      .replace(/-/g, '_')
+      .replace(/Wächter/g, 'Guardian')
+      .replace(/Vulkan/g, 'Ebony');
+
+    return `assets/MHWilds-Database-main/monster/assets/icons/${formattedName}_Icon.png`;
+  }
+
+  // Signal für das ausgewählte Monster
+  selectedMonster = signal<Monster | null>(null);
+
+  // Zugriff auf das Dialog-Element im HTML
+  monsterDialog = viewChild<ElementRef<HTMLDialogElement>>('monsterDialog');
+
+  openDetails(monster: Monster) {
+    this.selectedMonster.set(monster);
+    this.monsterDialog()?.nativeElement.showModal(); // Öffnet den Dialog nativ
+  }
+
+  closeDetails() {
+    this.monsterDialog()?.nativeElement.close();
+    this.selectedMonster.set(null);
+  }
 }
